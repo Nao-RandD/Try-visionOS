bcdbf0990cf0a2420ab719cbfe6c498ff5f5c659
Nao-RandD <nao.r.and.d@gmail.com>
2023-08-11 15:18:33 +0900

    first commit! :tada:

---
+++ Session1/Session1/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Session1
+//
+//  Created by Nao RandD on 2023/08/11.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .navigationTitle("Content")
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ Session1/Session1/Session1App.swift
@@ -0,0 +1,17 @@
+//
+//  Session1App.swift
+//  Session1
+//
+//  Created by Nao RandD on 2023/08/11.
+//
+
+import SwiftUI
+
+@main
+struct Session1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
