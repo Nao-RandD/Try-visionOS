d31909243db6195537f55832dd0e53d111883796
Nao-RandD <nao.r.and.d@gmail.com>
2023-09-16 22:06:09 +0900

    add: Add ObjectMaker Project for Vision Pro Developer Lab.

---
+++ VisionProLab/ObjectMakerApp/ObjectMakerApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ObjectMakerApp
+//
+//  Created by Nao RandD on 2023/09/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VisionProLab/ObjectMakerApp/ObjectMakerApp/ObjectMakerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectMakerAppApp.swift
+//  ObjectMakerApp
+//
+//  Created by Nao RandD on 2023/09/16.
+//
+
+import SwiftUI
+
+@main
+struct ObjectMakerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
