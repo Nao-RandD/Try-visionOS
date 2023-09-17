3ba615ffd787bf0a4ac1499176acf349fb597e4c
Nao-RandD <nao.r.and.d@gmail.com>
2023-09-17 12:17:44 +0900

    feat: Add Firebase settings in both projects.

---
+++ VisionProLab/ObjectMakerApp/Shared/AppDelegate.swift
@@ -0,0 +1,40 @@
+//
+//  AppDelegate.swift
+//  ObjectMakerApp
+//
+//  Created by Nao RandD on 2023/09/17.
+//
+
+import Foundation
+import FirebaseCore
+import FirebaseStorage
+import FirebaseFirestore
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+
+        setupFirebaseLocalEmulator()
+
+        return true
+    }
+
+    func setupFirebaseLocalEmulator() {
+        var host = "127.0.0.1"
+        #if !targetEnvironment(simulator)
+        host = "10.18.104.50"
+        #endif
+
+        // MARK: Firestore
+        let settings = Firestore.firestore().settings
+        settings.host = "\(host):8080"
+        settings.cacheSettings = MemoryCacheSettings()
+        settings.isSSLEnabled = false
+        Firestore.firestore().settings = settings
+
+        Storage.storage().useEmulator(withHost: host, port: 9199)
+    }
+}
+
